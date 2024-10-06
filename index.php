7fd5962aad4cf673e046ad052f4bf90b53eef816
shfwnirfndi <shafwanirfand261101@gmail.com>
2024-10-06 15:14:31 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto consequatur, accusantium perspiciatis repellat corporis totam magni vitae debitis velit quidem soluta dolore facere distinctio nemo nihil ipsa est quos nobis?</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
